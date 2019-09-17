e528e42f8d9f392f57590ce7844b32a0f5ce1072
Christian Ramos <christian-ruben1@hotmail.com>
2019-09-17 08:23:27 -0500

    Correcion de Clase Especialidad

--- src/main/java/com/hampcode/model/repository/EspecialidadRepository.java
+++ src/main/java/com/hampcode/model/repository/EspecialidadRepository.java
@@ -18,7 +18,7 @@ public class EspecialidadRepository implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
-	@PersistenceContext(unitName = "PwPU")
+	@PersistenceContext(unitName = "pwPU")
 	private EntityManager em;
 	
 	
