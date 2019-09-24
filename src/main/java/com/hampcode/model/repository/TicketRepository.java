ae673e0756c68a3f86783d9faca1394e469d654a
Chris <christian-ruben1@hotmail.com>
2019-09-24 09:39:08 -0500

    Implementando vista de Ticket

--- src/main/java/com/hampcode/model/repository/TicketRepository.java
+++ src/main/java/com/hampcode/model/repository/TicketRepository.java
@@ -13,56 +13,53 @@ import javax.persistence.TypedQuery;
 import com.hampcode.model.entity.Ticket;
 
 @Named
-public class TicketRepository implements Serializable{
+public class TicketRepository implements Serializable {
 
-private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-	@PersistenceContext(unitName="pwPU")
+	@PersistenceContext(unitName = "pwPU")
 	private EntityManager em;
-	
+
 	public Long insert(Ticket ticket) throws Exception {
 		em.persist(ticket);
 		return ticket.getIdTicket();
-	}	
-	
+	}
+
 	public Long update(Ticket ticket) throws Exception {
 		em.merge(ticket);
 		return ticket.getIdTicket();
 	}
-	
+
 	public void delete(Ticket ticket) throws Exception {
 		em.remove(ticket);
 	}
-	
-	public List<Ticket> findAll() throws Exception{
-		List<Ticket> tickets=new ArrayList<>();
-		
-		TypedQuery<Ticket> query=em.createQuery("FROM Ticket tic"
-				,Ticket.class);
-		tickets=query.getResultList();
-		
+
+	public List<Ticket> findAll() throws Exception {
+		List<Ticket> tickets = new ArrayList<>();
+
+		TypedQuery<Ticket> query = em.createQuery("FROM Ticket tic", Ticket.class);
+		tickets = query.getResultList();
+
 		return tickets;
 	}
-	
-	public Optional<Ticket> findById(Long id) throws Exception{
+
+	public Optional<Ticket> findById(Long id) throws Exception {
 		Ticket ticketFound;
-		
-		TypedQuery<Ticket> query=em.createQuery("FROM Ticket tic WHERE tic.id=?1"
-				,Ticket.class);
-		
+
+		TypedQuery<Ticket> query = em.createQuery("FROM Ticket tic WHERE tic.id=?1", Ticket.class);
+
 		query.setParameter(1, id);
-		ticketFound=query.getSingleResult();
-		
+		ticketFound = query.getSingleResult();
+
 		return Optional.of(ticketFound);
 	}
-		
-	public Ticket findByIdTicket(String id) throws Exception{
-		Ticket ticket=new Ticket();
-		TypedQuery<Ticket> query=em.createQuery("FROM Ticket tic WHERE tic.id=?1"
-				,Ticket.class);
-		query.setParameter(1, "%"+id+"%");
-		ticket=query.getSingleResult();
+
+	public Ticket findByIdTicket(String id) throws Exception {
+		Ticket ticket = new Ticket();
+		TypedQuery<Ticket> query = em.createQuery("FROM Ticket tic WHERE tic.id=?1", Ticket.class);
+		query.setParameter(1, "%" + id + "%");
+		ticket = query.getSingleResult();
 		return ticket;
 	}
-		
+
 }
